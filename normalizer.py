8acb99d3f46b82691fd4dd5f5f0d5e65996a9767
Harini S <harinisankaranarayanan06@gmail.com>
2026-06-04 14:34:29 +0530

    Delete normalizer.py

--- normalizer.py
+++
@@ -1,8 +0,0 @@
-def normalize(data):
-    normalized = {}
-    for k, v in data.items():
-        if isinstance(v, str):
-            normalized[k] = v.strip().lower()
-        else:
-            normalized[k] = v
-    return normalized
